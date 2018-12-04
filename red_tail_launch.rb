12154f574a643180264c8119bb2969010572ae24
Ryan Daly <ryandaly@Ryans-iMac.local>
2018-12-04 09:23:36 -0500

    setting autopilot to prograde vector

--- red_tail_launch.rb
+++ red_tail_launch.rb
@@ -7,23 +7,27 @@ vessel = sc.active_vessel
 ctrl = vessel.control
 
 require 'krpc'
-client = KRPC.connect(name: "sounding rocket script")
+client = KRPC.connect(name: "example")
 
 #setup
     sc = client.space_center
     vessel = sc.active_vessel
     ctrl = vessel.control
+    auto_pilot = vessel.auto_pilot
 
     vessel_pitch = ctrl.pitch 
     
 
     ctrl.activate_next_stage
-    sleep(5)
+    sleep(4)
     ctrl.activate_next_stage
 
     sleep until vessel.velocity >= 50
 
-       vessel.control.activate_next_stage ();
-        vessel.auto_pilot.engage ();
-        vessel.AutoPilot.TargetPitchAndHeading (90, 90);
+       
+    auto_pilot.reference_frame = vessel.surface_velocity_reference_frame
+    auto_pilot.target_direction = (0, 1, 0)
+    auto_pilot.engage() until vessel.thrust == 0 
+    
+
 
